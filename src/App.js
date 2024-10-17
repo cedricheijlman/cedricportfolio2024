a29ef68a34d9a2f83e8707f87e13963271069133
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2024-10-17 10:52:27 +0200

    React Setup , Navbar, Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<h1>home</h1>} />
+          <Route path="/my-work" element={<h1>Home</h1>} />
+          <Route path="/services" element={<h1>Services</h1>} />
+          <Route path="/about-me" element={<h1>Home</h1>} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./header.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <h1>
+          <Link to={"/"}>
+            <span>Cedric</span> <br /> Heijlman
+          </Link>
+        </h1>
+        <ul>
+          <li>
+            <Link to={"/my-work"}>My Work</Link>
+          </li>
+          <li>
+            <Link to={"/services"}>Services</Link>
+          </li>
+          <li>
+            <Link to={"/about-me"}>About Me</Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="header__right">
+        <button>Get in touch</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
